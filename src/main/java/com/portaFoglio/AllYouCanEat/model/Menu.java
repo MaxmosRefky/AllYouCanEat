5f68fc487bae78bf57cac6673b2e7005247f802c
MaxmosRefky <m.abdelsayed@contrader.it>
2023-12-28 15:19:58 +0100

    create menu model example

---
+++ src/main/java/com/portaFoglio/AllYouCanEat/model/Menu.java
@@ -0,0 +1,22 @@
+package com.portaFoglio.AllYouCanEat.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "menu")
+public class Menu {
+    @Id
+    @Column(name = "menu_id")
+    Integer menuId;
+
+
+}
